ff980302ef03f3f75bb0f294be52f40e5f1efb1c
용훈 황 <hyhhyhhyh@naver.com>
2022-10-14 11:17:50 +0900

    네트워크 통신
    TCP 기반 클라이언트 서버

---
+++ QTcpSocket/widget.h
@@ -0,0 +1,26 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QLabel>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void clientConnect();
+    void echoData();
+
+private:
+    QLabel *infoLabel;
+    QTcpServer *tcpServer;
+};
+#endif // WIDGET_H
