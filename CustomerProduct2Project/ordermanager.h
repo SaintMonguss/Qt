7dd6467b4f418dcbe43c1f22033d864de9131960
용훈 황 <kimmo1028@gmail.com>
2022-10-21 17:29:31 +0900

    오더 매니저 헤더 작성

--- CustomerProduct2Project/ordermanager.h
+++ CustomerProduct2Project/ordermanager.h
@@ -2,21 +2,46 @@
 #define ORDERMANAGER_H
 
 #include <QWidget>
+#include <QString>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <QMap>
 
-namespace Ui {
-class OrderManager;
-}
+#include "Order.h"
+#include "clientmanager.h"
+#include "productmanager.h"
 
-class OrderManager : public QWidget
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::map;
+
+class OrderManager : QWidget
 {
-    Q_OBJECT
+private:
+    QMap<int, Order*> orderList;
+    ClientManager* CM;
+    ProductManager* PM;
 
 public:
-    explicit OrderManager(QWidget *parent = nullptr);
+    OrderManager(ClientManager*, ProductManager*);
     ~OrderManager();
 
-private:
-    Ui::OrderManager *ui;
+private slots:
+    void on_productTreeWidget_itemClicked(QTreeWidgetItem *item, int column);
+    void resetSearchResult();
+
+public slots:
+    void AddObj();
+    void DelObj();
+    void ModiObj();
+    void SerchObj();
+    void PrintObj();
+    void TossObj(int);
+    void printOrderForm(QMap<int, Order*> &) const;
 };
 
 #endif // ORDERMANAGER_H
